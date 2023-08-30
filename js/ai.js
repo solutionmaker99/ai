c5642f65e80fc4e18da0ba03737556e1365ba349
solugionmaker99 <frriyad111@gmail.com>
2023-08-30 22:11:18 +0600

    second commit

--- js/ai.js
+++ js/ai.js
@@ -1,1 +1,15 @@
-console.log("hello riyad");
+loadData = async () => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
+  const data = await res.json();
+  const cards = data.data.tools;
+  console.log(cards);
+  displayCards(cards);
+};
+
+displayCards = (cards) => {
+  cards.forEach((card) => {
+    console.log(card);
+  });
+};
+
+loadData();
